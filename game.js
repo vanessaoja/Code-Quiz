5785cf92ce4faa89586c4742fa2c94d8c2f4436a
vanessaoja <ojavanessa@gmail.com>
2022-06-23 17:46:46 -0500

    add question

---
+++ game.js
@@ -0,0 +1,17 @@
+const question = document.querySelector("#question");
+const choices= Array.from(document.querySelector(".choice-text"));
+const progressText = document.querySelector("#progressText");
+const scoreText = document.querySelector("#score");
+const progressBarFull = document.querySelector("#progressBarFull");
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
+
+let questions = [
+    {
+        question: "What type of language is Javascript?"
+    }
+]
