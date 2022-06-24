72475315f82c8f3597b63c75c7af8facb29c482e
vanessaoja <ojavanessa@gmail.com>
2022-06-23 21:18:45 -0500

    question counter

--- game.js
+++ game.js
@@ -12,6 +12,30 @@ let availableQuestions = []
 
 let questions = [
     {
-        question: "What type of language is Javascript?"
+        question: "What type of language is Javascript?",
+        choice1: 'Object-Oriented' ,
+        choice2: 'Object-Based' ,
+        choice3: 'Procedural' ,
+        choice4: 'None of the above' ,
+        answer: 1,
     }
 ]
+const score_points = 100
+const max_questions = 1
+
+startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = (...questions)
+    getNewQuesiton()
+}
+
+getNewQuesiton = () => {
+    if(availableQuestions.length === 0 || quesitonsCounter > max_questions) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('/end.html')
+    }
+    quesitonCounter++
+    progressText.innerText = "Question ${questionCounter} of ${max_questions}"
+}
